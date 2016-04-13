2efedabeeae8386bbdecd93fde764e48fcff2a12
samantonio <samueltussing@alum.calarts.edu>
2016-04-13 16:56:47 -0500

    Ant World 3D a4.20

---
+++ Assets/Camera/CamFollow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFollow : MonoBehaviour {
+
+
+
+	//to move camera
+	public Transform target;
+	//add some natural motion
+	public float smoothing = 5f;
+
+
+
+	//creates a variable to determine distance from cam to player
+	Vector3 offset;
+
+	//defines distance from camera to player
+	void Start()
+	{
+		offset = transform.position - target.position;
+	}
+
+
+	void FixedUpdate()
+	{
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+ 
+}
